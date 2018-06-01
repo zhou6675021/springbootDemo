84ef3cfbeadf674726aacc9036bc2f68993a3fa9
zhou6675021 <hbwxzxl948820800@163.com>
2018-06-01 16:37:57 +0800

    11

---
+++ eurekaconsumerfeign/src/main/java/src/controller/DcController.java
@@ -0,0 +1,20 @@
+package src.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    DcClient dcClient;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return dcClient.consumer();
+    }
+}
